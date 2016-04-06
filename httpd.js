71cf2571f043c212df76463223509cfce626ca4f
Qiu Shan Liu <qsliubj@cn.ibm.com>
2016-04-06 10:46:07 +0800

    first commit

---
+++ httpd.js
@@ -0,0 +1,29 @@
+/**
+ * Created by qiushan on 4/5/2016.
+ */
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var exec = require('child_process').exec;
+
+app.get('/', function(req, res){
+    res.send('<h1>z-Cluster Realtime Server</h1>');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on("deploy", function(data) {
+        var term = data.toLowerCase();
+        console.log("deploy term: " + term);
+        var child = exec('echo hello ' + term, function(err, stdout, stderr) {
+            if (err) throw err;
+            console.log(stdout);
+        });
+    });
+});
+
+http.listen(10002, function(){
+    console.log('listening on *:10002');
+});
