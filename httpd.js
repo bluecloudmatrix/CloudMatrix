8b076dab9996770d4fbdfd2c0c1f03a51f7d978a
root <bluecloudmatrix@gmail.com>
2016-04-06 22:39:51 -0400

    shelljs

--- httpd.js
+++ httpd.js
@@ -6,7 +6,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var exec = require('child_process').exec;
+//var exec = require('child_process').exec;
+
+var shell = require("shelljs");
 
 app.get('/', function(req, res){
     res.send('<h1>z-Cluster Realtime Server</h1>');
@@ -17,10 +19,11 @@ io.on('connection', function(socket){
     socket.on("deploy", function(data) {
         var term = data.toLowerCase();
         console.log("deploy term: " + term);
-        var child = exec('echo hello ' + term, function(err, stdout, stderr) {
-            if (err) throw err;
-            console.log(stdout);
-        });
+        //var child = exec('echo hello ' + term, function(err, stdout, stderr) {
+        //    if (err) throw err;
+        //    console.log(stdout);
+        //});
+        shell.exec("echo hello " + term);
     });
 });
 
