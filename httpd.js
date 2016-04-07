70f461242365aa5ede4d5f77f364dacbd89eefba
Monkey.D.Luffy <bluecloudmatrix@gmail.com>
2016-04-07 13:39:21 +0800

    5th

--- httpd.js
+++ httpd.js
@@ -23,7 +23,7 @@ io.on('connection', function(socket){
         //    if (err) throw err;
         //    console.log(stdout);
         //});
-        shell.exec("echo jekins hello forth time " + term);
+        shell.exec("echo jekins hello fifth time " + term);
     });
 });
 
