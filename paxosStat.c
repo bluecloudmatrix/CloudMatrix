129eb0ac247245b190fd4fd2deff46d3d4634581
root <bluecloudmatrix@gmail.com>
2016-04-28 01:31:08 -0400

    paxos

---
+++ paxosStat.c
@@ -0,0 +1,19 @@
+/*
+ *
+ * ======================================================
+ *
+ *  Qiushan
+ *  to do fault tolerance 
+ *
+ *  28/04/2016
+ *
+ *  =====================================================
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+	proxy_run();
+}
